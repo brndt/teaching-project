b90ce5e8a751d295376089b0435d202eef8c7122
Belacqua <belacquarium@gmail.com>
2020-04-16 17:25:06 +0200

    Login and Refresh Token controllers update

--- src/User/Infrastructure/Framework/Controller/LoginController.php
+++ src/User/Infrastructure/Framework/Controller/LoginController.php
@@ -6,13 +6,101 @@ namespace LaSalle\StudentTeacher\User\Infrastructure\Framework\Controller;
 
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Gesdinet\JWTRefreshTokenBundle\Model\RefreshTokenInterface;
+use Gesdinet\JWTRefreshTokenBundle\Model\RefreshTokenManagerInterface;
+use Gesdinet\JWTRefreshTokenBundle\Request\RequestRefreshToken;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 final class LoginController extends AbstractFOSRestController
 {
+    protected JWTTokenManagerInterface $jwtManager;
+    private RefreshTokenManagerInterface $refreshTokenManager;
+    private $ttl;
+    private ValidatorInterface $validator;
+    private RequestStack $requestStack;
+    private $userIdentityField;
+    private $tokenParameterName;
+    private $singleUse;
+
+    public function __construct(
+        JWTTokenManagerInterface $jwtManager,
+        RefreshTokenManagerInterface $refreshTokenManager,
+        $ttl,
+        ValidatorInterface $validator,
+        RequestStack $requestStack,
+        $userIdentityField,
+        $tokenParameterName,
+        $singleUse
+    ) {
+        $this->jwtManager = $jwtManager;
+        $this->refreshTokenManager = $refreshTokenManager;
+        $this->ttl = $ttl;
+        $this->validator = $validator;
+        $this->requestStack = $requestStack;
+        $this->userIdentityField = $userIdentityField;
+        $this->tokenParameterName = $tokenParameterName;
+        $this->singleUse = $singleUse;
+    }
+
     /**
      * @Rest\Post("/api/sign_in", name="sign_in")
      */
-    public function postAction()
+    public function postAction($jwt = null)
     {
+        if (null === $jwt) {
+            $jwt = $this->jwtManager->create($this->getUser());
+        }
+        $data['token'] = $jwt;
+
+        $request = $this->requestStack->getCurrentRequest();
+
+        $refreshTokenString = RequestRefreshToken::getRefreshToken($request, $this->tokenParameterName);
+
+        if ($refreshTokenString && true === $this->singleUse) {
+            $refreshToken = $this->refreshTokenManager->get($refreshTokenString);
+            $refreshTokenString = null;
+
+            if ($refreshToken instanceof RefreshTokenInterface) {
+                $this->refreshTokenManager->delete($refreshToken);
+            }
+        }
+
+        if ($refreshTokenString) {
+            $data[$this->tokenParameterName] = $refreshTokenString;
+        } else {
+            $datetime = new \DateTime();
+            $datetime->modify('+' . $this->ttl . ' seconds');
+
+            $refreshToken = $this->refreshTokenManager->create();
+
+            $accessor = new PropertyAccessor();
+            $userIdentityFieldValue = $accessor->getValue($this->getUser(), $this->userIdentityField);
+
+            $refreshToken->setUsername($userIdentityFieldValue);
+            $refreshToken->setRefreshToken();
+            $refreshToken->setValid($datetime);
+
+            $valid = false;
+            while (false === $valid) {
+                $valid = true;
+                $errors = $this->validator->validate($refreshToken);
+                if ($errors->count() > 0) {
+                    foreach ($errors as $error) {
+                        if ('refreshToken' === $error->getPropertyPath()) {
+                            $valid = false;
+                            $refreshToken->setRefreshToken();
+                        }
+                    }
+                }
+            }
+
+            $this->refreshTokenManager->save($refreshToken);
+            $data[$this->tokenParameterName] = $refreshToken->getRefreshToken();
+        }
+
+        return $data;
     }
 }
--- src/User/Infrastructure/Framework/Controller/RefreshTokenController.php
+++ src/User/Infrastructure/Framework/Controller/RefreshTokenController.php
@@ -6,16 +6,81 @@ namespace LaSalle\StudentTeacher\User\Infrastructure\Framework\Controller;
 
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use Gesdinet\JWTRefreshTokenBundle\Service\RefreshToken;
+use Gesdinet\JWTRefreshTokenBundle\Model\RefreshTokenManagerInterface;
+use Gesdinet\JWTRefreshTokenBundle\Security\Authenticator\RefreshTokenAuthenticator;
+use Gesdinet\JWTRefreshTokenBundle\Security\Provider\RefreshTokenProvider;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
 
 final class RefreshTokenController extends AbstractFOSRestController
 {
+    private RefreshTokenAuthenticator $authenticator;
+    private RefreshTokenProvider $provider;
+    private AuthenticationSuccessHandlerInterface $successHandler;
+    private AuthenticationFailureHandlerInterface $failureHandler;
+    private RefreshTokenManagerInterface $refreshTokenManager;
+    private $ttl;
+    private $providerKey;
+    private $ttlUpdate;
+
+    public function __construct(
+        RefreshTokenAuthenticator $authenticator,
+        RefreshTokenProvider $provider,
+        AuthenticationSuccessHandlerInterface $successHandler,
+        AuthenticationFailureHandlerInterface $failureHandler,
+        RefreshTokenManagerInterface $refreshTokenManager,
+        $ttl,
+        $providerKey,
+        $ttlUpdate
+    ) {
+        $this->authenticator = $authenticator;
+        $this->provider = $provider;
+        $this->successHandler = $successHandler;
+        $this->failureHandler = $failureHandler;
+        $this->refreshTokenManager = $refreshTokenManager;
+        $this->ttl = $ttl;
+        $this->providerKey = $providerKey;
+        $this->ttlUpdate = $ttlUpdate;
+    }
+
     /**
      * @Rest\Post("/api/refresh_token", name="token_refresh")
      */
-    public function postAction(Request $request, RefreshToken $refreshToken)
+    public function postAction(Request $request)
     {
-        return $refreshToken->refresh($request);
+        try {
+            $user = $this->authenticator->getUser(
+                $this->authenticator->getCredentials($request),
+                $this->provider
+            );
+
+            $postAuthenticationToken = $this->authenticator->createAuthenticatedToken($user, $this->providerKey);
+        } catch (AuthenticationException $e) {
+            return $this->failureHandler->onAuthenticationFailure($request, $e);
+        }
+
+        $credentials = $this->authenticator->getCredentials($request);
+        $refreshToken = $this->refreshTokenManager->get($credentials['token']);
+
+        if (null === $refreshToken || !$refreshToken->isValid()) {
+            return $this->failureHandler->onAuthenticationFailure(
+                $request,
+                new AuthenticationException(
+                    sprintf('Refresh token "%s" is invalid.', $refreshToken)
+                )
+            );
+        }
+
+        if ($this->ttlUpdate) {
+            $expirationDate = new \DateTime();
+            $expirationDate->modify(sprintf('+%d seconds', $this->ttl));
+            $refreshToken->setValid($expirationDate);
+
+            $this->refreshTokenManager->save($refreshToken);
+        }
+
+        return $this->successHandler->onAuthenticationSuccess($request, $postAuthenticationToken);
     }
 }
