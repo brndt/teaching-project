7fe74fbfb4f6d9206e9a9686570f4c3b3b030688
deock <irving.cruz@students.salle.url.edu>
2020-05-31 16:09:48 +0200

    Added nelmio_cors library

--- src/config/bundles.php
+++ src/config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
