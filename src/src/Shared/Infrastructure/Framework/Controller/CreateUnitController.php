3242f39a3ddbbb0025f6b081576bdb28feffdb66
deock <irving.cruz@students.salle.url.edu>
2020-09-02 21:43:00 +0200

    Adding Create Unit Controller

---
+++ src/src/Shared/Infrastructure/Framework/Controller/CreateUnitController.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace LaSalle\StudentTeacher\Shared\Infrastructure\Framework\Controller;
+
+
+use DateTimeImmutable;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\Annotations\RequestParam;
+use FOS\RestBundle\Request\ParamFetcher;
+use LaSalle\StudentTeacher\Resource\Application\Request\CreateUnitRequest;
+use LaSalle\StudentTeacher\Resource\Application\Service\CreateUnitService;
+use Symfony\Component\HttpFoundation\Response;
+
+class CreateUnitController extends AbstractFOSRestController
+{
+    private CreateUnitService $createUnitService;
+
+    public function __construct(CreateUnitService $createUnitService)
+    {
+        $this->createUnitService = $createUnitService;
+    }
+
+    /**
+     * @Rest\Post("/api/v1/panel/units")
+     * @RequestParam(name="courseId")
+     * @RequestParam(name="name")
+     * @RequestParam(name="description", nullable=true)
+     * @RequestParam(name="level")
+     * @RequestParam(name="status")
+     * @param ParamFetcher $paramFetcher
+     * @return Response
+     */
+    public function postAction(ParamFetcher $paramFetcher): Response
+    {
+        $requestAuthorId = $this->getUser()->getId();
+        $courseId = $paramFetcher->get('courseId');
+        $name = $paramFetcher->get('name');
+        $description = $paramFetcher->get('description');
+        $level = $paramFetcher->get('level');
+        $status = $paramFetcher->get('status');
+
+        ($this->createUnitService)(new CreateUnitRequest($requestAuthorId, $courseId, $name, $description, $level, new DateTimeImmutable(), null, $status));
+
+        return $this->handleView(
+            $this->view(['message' => 'Unit has been successfully created'], Response::HTTP_CREATED)
+        );
+    }
+
+}
